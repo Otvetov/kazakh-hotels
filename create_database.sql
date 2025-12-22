2ef2411d13d8949385101a3aa39fba8a52576d6b
Roman Slushniy <roman07062007sluhny@gmail.com>
2025-12-22 16:52:18 +0500

    docs: add GitHub setup and commit reorganization guides

---
+++ create_database.sql
@@ -0,0 +1,7 @@
+-- SQL скрипт для создания базы данных
+-- Выполните этот скрипт в phpMyAdmin или через MySQL командную строку
+
+CREATE DATABASE IF NOT EXISTS `kazakh_hotel` 
+CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
+
